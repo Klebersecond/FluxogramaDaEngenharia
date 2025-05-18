41d266fdd51d2f041c7127e751950910cb48d415
Kleber Neves <klebernssii@gmail.com>
2025-05-18 15:54:30 -0300

    Finaliza Programa

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Write("Fluxograma da engenharia");
+Console.BackgroundColor = ConsoleColor.Green;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.ResetColor();
+
+Console.Write($"Está se movendo (S/N)?:");
+string estamovendo = Console.ReadLine()!.Trim().ToUpper();
+
+Console.Write("Deveria (S/N) ?:");
+string deveria = Console.ReadLine()!.Trim().ToUpper();
+
+if (estamovendo == "S" && deveria == "N")
+
+{
+    Console.WriteLine("Use silver tape");
+}
+
+else if (estamovendo == "N" && deveria == "S")
+
+{
+    Console.WriteLine("Use WD-40");
+}
+
+else
+{
+    Console.WriteLine("Òtimo!");
+}
+
+
+
+
